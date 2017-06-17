97d38701a7412d0c3c6a9370190ba719a40aff93
croog24 <croog24@gmail.com>
2017-06-17 19:38:57 -0400

    Moving Google APIs to different project

--- Park-A-Lot_Server/src/main/java/com/github/parkalot/AppContext.java
+++ Park-A-Lot_Server/src/main/java/com/github/parkalot/AppContext.java
@@ -2,22 +2,12 @@ package com.github.parkalot;
 
 import org.lightcouch.CouchDbClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.github.parkalot")
-@PropertySource("classpath:config.properties")
 @EnableWebMvc
 public class AppContext {
-
-    @Bean(name = "googlePlacesRestTemplate")
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
 
     @Bean
     public CouchDbClient getCouchDbClient() {
--- Park-A-Lot_Server/src/test/java/com/github/parkalot/TestContext.java
+++ Park-A-Lot_Server/src/test/java/com/github/parkalot/TestContext.java
@@ -1,8 +1,14 @@
 package com.github.parkalot;
 
+import org.lightcouch.CouchDbClient;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 
 @TestConfiguration
-public class TestContext extends AbstractTestContext {
-
+@ComponentScan("com.github.parkalot")
+public class TestContext {
+    
+    @MockBean
+    private CouchDbClient couchDbCLient;
 }
