529838a778d196bc076804f8faf2114636d3a493
croog24 <croog24@gmail.com>
2017-06-17 16:40:48 -0400

    Missed Bean annotations in TestContext

--- Park-A-Lot_Server/src/test/java/com/github/parkalot/AbstractTestContext.java
+++ Park-A-Lot_Server/src/test/java/com/github/parkalot/AbstractTestContext.java
@@ -2,6 +2,7 @@ package com.github.parkalot;
 
 import org.lightcouch.CouchDbClient;
 import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @PropertySource("classpath:testConfig.properties")
 public class AbstractTestContext {
 
+    @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
+    @Bean
     public CouchDbClient getCouchDbClient() {
         return Mockito.mock(CouchDbClient.class);
     }
