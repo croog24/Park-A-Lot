40d63d9edfd3010dc073a6021a6121730a7f7ce8
croog24 <croog24@gmail.com>
2017-06-17 21:33:48 -0400

    Fixed Controller annotation

--- Park-A-Lot_Server/src/main/java/com/github/parkalot/controller/ParkingLotController.java
+++ Park-A-Lot_Server/src/main/java/com/github/parkalot/controller/ParkingLotController.java
@@ -1,11 +1,10 @@
 package com.github.parkalot.controller;
 
-import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,11 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.parkalot.model.ParkingLot;
 import com.github.parkalot.service.ParkingLotService;
 
-@Controller
+@RestController
 @RequestMapping("/parking-lot/{parking-lot-id}")
 public class ParkingLotController {
 
